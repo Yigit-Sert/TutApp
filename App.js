27e5646215c9e9341da504b361dbacac18952d38
Yiğit <yigitsert32@gmail.com>
2023-01-01 20:16:17 +0300

    basic delete function is added

--- App.js
+++ App.js
@@ -18,13 +18,22 @@ export default function App() {
       [...currenCourseGoals, { text: enteredGoalText, id: Math.random().toString() }]));
   }
 
+  function deleteGoalHandler(id) {
+    setCourseGoals(currenCourseGoals => {
+      return currenCourseGoals.filter((goal) => goal.id !== id);
+    });
+  }
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         {/* use flatlist to have more optimized scrolling instead of scrollview */}
         <FlatList data={courseGoals} renderItem={(itemData) => {
-          return <GoalItem text={itemData.item.text} />;
+          return <GoalItem
+            text={itemData.item.text}
+            id={itemData.item.id}
+            onDeleteItem={deleteGoalHandler} />;
         }}
           keyExtractor={(item, index) => {
             return item.id;
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,12 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
     return (
-        <View style={styles.goalsItem}>
-            <Text style={styles.goalText}>{props.text}</Text>
-        </View>
+        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+            <View style={styles.goalsItem}>
+                <Text style={styles.goalText}>{props.text}</Text>
+            </View>
+        </Pressable>
     );
 };
 
